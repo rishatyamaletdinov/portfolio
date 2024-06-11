f18c5f0ce9545636cd6c262a21b9e84f88286df9
Rishat Yamaletdinov <yarishat8@gmail.com>
2024-06-11 16:18:48 +0500

    feat: actionCardSkill function added

--- js/cards.js
+++ js/cards.js
@@ -1,11 +1,18 @@
 const sectionSkills = document.querySelector("#section-skills");
 
-function openCardSkill(event) {
+function actionCardSkill(event) {
     const card = event.target.closest("[data-card-skill]");
+    const closeCross = event.target.closest("[card-close-cross]");
+
     if (card) {
         card.classList.add("active");
         document.body.classList.add("active");
     }
+
+    if (closeCross) {
+        card.classList.remove("active");
+        document.body.classList.remove("active");
+    }
 }
 
-sectionSkills.addEventListener("click", openCardSkill);
+sectionSkills.addEventListener("click", actionCardSkill);
