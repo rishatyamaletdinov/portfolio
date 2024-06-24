9518f9d731a4f7ec60af036da39996df99ce1de2
Rishat Yamaletdinov <yarishat8@gmail.com>
2024-06-24 22:27:15 +0500

    feat: reviews section animated

--- js/gsap-animation.js
+++ js/gsap-animation.js
@@ -34,3 +34,17 @@ gsap.from('[data-card-project]',{
         // toggleActions: 'play none none reverse',
     }
 })
+
+gsap.from('[data-slider]',{
+    stagger:0.3,
+    opacity:0,
+    y:200,
+    scrollTrigger:{
+        // markers:true,
+        trigger: '#section-reviews',
+        start: 'top 70%',
+        end:'30% 50%',
+        scrub: 2,
+        // toggleActions: 'play none none reverse',
+    }
+})
