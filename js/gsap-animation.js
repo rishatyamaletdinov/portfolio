7cfe36df23f59925cb88faa0b85350d3eb6273ea
Rishat Yamaletdinov <yarishat8@gmail.com>
2024-06-25 16:04:45 +0500

    picture avatar animated

--- js/gsap-animation.js
+++ js/gsap-animation.js
@@ -48,3 +48,47 @@ gsap.from('[data-slider]',{
         // toggleActions: 'play none none reverse',
     }
 })
+
+function paralax() {
+    const preview = document.querySelector('#welcome');
+    const initialX = preview.offsetLeft + preview.offsetWidth / 2;
+    const initialY = preview.offsetTop + preview.offsetHeight / 2;
+
+    document.addEventListener('mousemove', function(event){
+    const mouseX = event.clientX - initialX;
+    const mouseY = event.clientY - initialY;
+    
+    
+    gsap.to(
+        '#picture-avatar-one', 
+        {
+            rotationX: -mouseX * 0.02, 
+            rotationY: mouseY * 0.02, 
+            x:-mouseX * 0.02,
+            y: mouseY * 0.02,
+            ease: 'power2.out' ,
+        },);
+        
+    gsap.to(
+        '#picture-avatar-two', 
+        {
+            rotationX: mouseX * 0.025, 
+            rotationY: -mouseY * 0.025,
+            x:-mouseX * 0.02,
+            y: mouseY * 0.02, 
+            ease: 'power2.out' ,
+        },);
+        
+        gsap.to(
+        '#picture-avatar-photo', 
+        {
+            rotationX: -mouseX * 0.025, 
+            rotationY: -mouseY * 0.025, 
+            x:-mouseX * 0.02,
+            y: mouseY * 0.02,
+            ease: 'power2.out' ,
+        },);
+    }) 
+}
+
+paralax()
