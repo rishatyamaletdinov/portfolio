b8bfbbc72af416970e08bb3cce5c6d144876e69c
Rishat Yamaletdinov <yarishat8@gmail.com>
2024-07-09 16:24:42 +0500

    feat: blur animation for inputs added

--- js/placeholder-animation.js
+++ js/placeholder-animation.js
@@ -1,12 +1,31 @@
 const clientForm = document.querySelector("#form")
 
-const placeholderFocusAnimation = (e) => {
+const setPlaceholderFocus = (e) => {
     const isInputField = e.target.closest("[data-input-field]")
     if (isInputField) {
         const formItem = isInputField.closest("[data-form-item]");
         const fakePlaceholder = formItem.querySelector("[data-fake-placeholder]");
         fakePlaceholder.classList.add("focus");
+
+        isInputField.addEventListener('blur', setPlaceholderBlur);
     }
 }
 
-clientForm.addEventListener("click", placeholderFocusAnimation);
+const setPlaceholderBlur = (e) => {
+
+    const isInputField = e.target.closest("[data-input-field]")
+    if (isInputField) {
+        const formItem = isInputField.closest("[data-form-item]");
+        const fakePlaceholder = formItem.querySelector("[data-fake-placeholder]");
+        if (isInputField.value.trim().length < 1) {
+            fakePlaceholder.classList.remove("focus");
+        }
+        isInputField.removeEventListener('blur', setPlaceholderBlur);
+    }
+
+}
+
+clientForm.addEventListener("click", setPlaceholderFocus);
+
+
+// добавить проверку на change или события input
